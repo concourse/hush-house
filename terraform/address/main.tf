a1397339083b1de17f2be6c001af1619e5e34974
Krishna Mannem <kmannem@pivotal.io>
2019-11-06 13:16:42 -0500

    Update terraform address mod dns name
    Co-authored-by: Sameer Vohra <svohra@pivotal.io>

--- terraform/address/main.tf
+++ terraform/address/main.tf
@@ -6,7 +6,7 @@ resource "google_compute_address" "main" {
   name = "${var.subdomain}"
 }
 
-resource "google_dns_record_set" "metrics" {
+resource "google_dns_record_set" "main" {
   name = "${var.subdomain}.${data.google_dns_managed_zone.main.dns_name}"
   type = "A"
   ttl  = 300
