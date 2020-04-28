b32e895b9dac6f96121fd4cc60ea3379985e6456
Alex Suraci <asuraci@pivotal.io>
2020-04-28 12:40:48 -0400

    add outputs to cluster module
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/cluster/outputs.tf
+++ terraform/cluster/outputs.tf
@@ -1,3 +1,20 @@
 output "vpc_uri" {
   value = "${module.vpc.uri}"
 }
+
+output "host" {
+  value = "https://${google_container_cluster.main.endpoint}"
+}
+
+output "client_certificate" {
+  value = google_container_cluster.master_auth[0].client_certificate
+}
+
+output "client_key" {
+  sensitive = true
+  value = google_container_cluster.master_auth[0].client_key
+}
+
+output "cluster_ca_certificate" {
+  value = google_container_cluster.master_auth[0].cluster_ca_certificate
+}
--- terraform/production/helm.tf
+++ terraform/production/helm.tf
@@ -1,9 +1,9 @@
 provider "helm" {
   kubernetes {
-    host     = "https://${module.cluster.google_container_cluster.main.endpoint}"
+    host     = module.cluster.host
 
-    client_certificate     = module.cluster.google_container_cluster.master_auth[0].client_certificate
-    client_key             = module.cluster.google_container_cluster.master_auth[0].client_key
-    cluster_ca_certificate = module.cluster.google_container_cluster.master_auth[0].cluster_ca_certificate
+    client_certificate     = module.cluster.client_certificate
+    client_key             = module.cluster.client_key
+    cluster_ca_certificate = module.cluster.cluster_ca_certificate
   }
 }
