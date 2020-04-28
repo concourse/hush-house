7f7e8b8b5ef8da335e425e42ad6bfb0ee16a7624
Alex Suraci <asuraci@pivotal.io>
2020-04-28 13:08:50 -0400

    base64decode cluster CA certificate
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/cluster/outputs.tf
+++ terraform/cluster/outputs.tf
@@ -16,5 +16,5 @@ output "password" {
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.master_auth[0].cluster_ca_certificate
+  value = base64decode(google_container_cluster.master_auth[0].cluster_ca_certificate)
 }
