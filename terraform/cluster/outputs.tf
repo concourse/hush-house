8e9689b6e73b0114c1b76e785700ff502b3590b7
Alex Suraci <asuraci@pivotal.io>
2020-04-28 15:27:07 -0400

    main
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/cluster/outputs.tf
+++ terraform/cluster/outputs.tf
@@ -7,14 +7,14 @@ output "endpoint" {
 }
 
 output "username" {
-  value = google_container_cluster.master_auth[0].username
+  value = google_container_cluster.main.master_auth[0].username
 }
 
 output "password" {
-  value = google_container_cluster.master_auth[0].password
+  value = google_container_cluster.main.master_auth[0].password
   sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.master_auth[0].cluster_ca_certificate
+  value = google_container_cluster.main.master_auth[0].cluster_ca_certificate
 }
