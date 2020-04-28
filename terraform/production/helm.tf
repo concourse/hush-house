4abb9f6ab0fc2f43070eadf572f37bb2d9a98969
Alex Suraci <asuraci@pivotal.io>
2020-04-28 14:41:59 -0400

    use kubeconfig instead of static config
    static config doesn't give useful errors, so let's try this
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- terraform/production/helm.tf
+++ terraform/production/helm.tf
@@ -1,10 +1,22 @@
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/kubeconfig-template.yml")
+
+  vars = {
+    cluster_name    = module.cluster.name
+    user_name       = module.cluster.username
+    user_password   = module.cluster.password
+    endpoint        = module.cluster.endpoint
+    cluster_ca      = module.cluster.cluster_ca_certificate
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = data.template_file.kubeconfig.rendered
+  filename = "${path.module}/kubeconfig"
+}
+
 provider "helm" {
   kubernetes {
-    load_config_file = false
-
-    host  = "https://${module.cluster.endpoint}"
-    token = data.google_client_config.current.access_token
-
-    cluster_ca_certificate = base64decode(module.cluster.cluster_ca_certificate)
+    config_path = "${path.module}/kubeconfig"
   }
 }
