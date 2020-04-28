193c9fcd012bb6908badc3bd411d1750d98c5a2e
Alex Suraci <asuraci@pivotal.io>
2020-04-28 15:35:07 -0400

    attempt static configuration for helm once more
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/production/helm.tf
+++ terraform/production/helm.tf
@@ -1,5 +1,11 @@
 provider "helm" {
   kubernetes {
-    config_path = local_file.kubeconfig.filename
+    load_config_file = false
+
+    host     = "https://${module.cluster.endpoint}"
+    username = module.cluster.username
+    password = module.cluster.password
+
+    cluster_ca_certificate = base64decode(module.cluster.cluster_ca_certificate)
   }
 }
