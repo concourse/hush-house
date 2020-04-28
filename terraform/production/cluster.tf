7c22a0f72bf5d26406226b3a2a733f0efd6c60e4
Alex Suraci <asuraci@pivotal.io>
2020-04-28 15:47:31 -0400

    add generic-1 node pool
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/production/cluster.tf
+++ terraform/production/cluster.tf
@@ -9,5 +9,17 @@ module "cluster" {
 
   release_channel = "STABLE"
 
-  node_pools = {}
+  node_pools = {
+    "generic-1" = {
+      auto_upgrade = true
+      disk_size    = "50"
+      disk_type    = "pd-ssd"
+      image        = "COS"
+      local_ssds   = 0
+      machine_type = "n1-standard-8"
+      max          = 5
+      min          = 1
+      preemptible  = false
+    },
+  }
 }
