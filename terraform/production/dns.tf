d97f8ff27c237a90cecda19ec33a6eb7afde8ea5
Alex Suraci <asuraci@pivotal.io>
2020-04-30 11:34:29 -0400

    remove terraform_remote_state for now
    we need a root-level output in order to access the dns zone.
    for now, just use a variable
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/production/dns.tf
+++ terraform/production/dns.tf
@@ -1,12 +1,15 @@
-data "terraform_remote_state" "iaas" {
-  backend = "gcs"
-  config = {
-    // Do we need credentials + project + region here, or will it inherit from
-    // the provider we defined in gcp.tf?
-    bucket = "concourse-tf-state"
-    prefix = "terraform/state"
-  }
-}
+# Leaving commented out for now. We'll need to expose the dns name of our DNS managed zone
+# in order to access it through terraform_remote_state.
+#
+# data "terraform_remote_state" "iaas" {
+#   backend = "gcs"
+#   config = {
+#     // Do we need credentials + project + region here, or will it inherit from
+#     // the provider we defined in gcp.tf?
+#     bucket = "concourse-tf-state"
+#     prefix = "terraform/state"
+#   }
+# }
 
 # Reserves an address for `ci-test.concourse-ci.org` and ties it
 # to the given domain.
@@ -14,6 +17,6 @@ data "terraform_remote_state" "iaas" {
 module "concourse_ci_address" {
   source = "../address"
 
-  dns_zone  = "${data.terraform_remote_state.iaas.google_dns_managed_zone.concourse-ci-org.dns_name}"
+  dns_zone  = var.dns_zone
   subdomain = "ci-test"
 }
--- terraform/production/variables.tf
+++ terraform/production/variables.tf
@@ -17,3 +17,8 @@ variable "project" {
   description = "The Google GCP project to host the resources."
   default     = "cf-concourse-production"
 }
+
+variable "dns_zone" {
+  description = "The default DNS zone to use when creating subdomains."
+  default     = "concourse-ci-org"
+}
