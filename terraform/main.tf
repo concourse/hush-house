16f52e57c273282ebace68051b0fe9133dc3a04e
Rui Yang <ruiya@vmware.com>
2023-01-05 13:05:42 -0500

    update workers machine
    these setting turns out to solve the error
    `no running task found` caused by mem kill
    
    Signed-off-by: Rui Yang <ruiya@vmware.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ module "cluster" {
       disk-type    = "pd-ssd"
       image        = "UBUNTU"
       local-ssds   = 0
-      machine-type = "e2-highcpu-8"
+      machine-type = "e2-highmem-4"
       max          = 25
       min          = 1
       preemptible  = false
